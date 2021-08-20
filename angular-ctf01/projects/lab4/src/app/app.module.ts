499fe03b7ee82c210d40d347aaae32d1d82c47ae
Patcharin <gookikps@gmail.com>
2021-08-20 15:26:21 +0700

    commit

--- angular-ctf01/projects/lab4/src/app/app.module.ts
+++ angular-ctf01/projects/lab4/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Lab4Component } from './lab4component/lab4.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Lab4Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
