a49955c3783a264bee4528c5b06eceb743ad2dd5
Patcharin <gookikps@gmail.com>
2021-08-20 18:10:17 +0700

    commit

--- angular-ctf01/projects/hw1/src/app/app.module.ts
+++ angular-ctf01/projects/hw1/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
