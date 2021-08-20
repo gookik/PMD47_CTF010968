9b06ce6e4be703638259c66daa966668573c8328
Patcharin <gookikps@gmail.com>
2021-08-20 15:53:49 +0700

    commit

--- angular-ctf01/projects/lab4/src/app/lab4component/lab4.component.ts
+++ angular-ctf01/projects/lab4/src/app/lab4component/lab4.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
 selector: 'app-lab4component',
 templateUrl: './lab4.component.html'
 })
 export class Lab4Component {
+    
+    item = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
 constructor() {}
 }
