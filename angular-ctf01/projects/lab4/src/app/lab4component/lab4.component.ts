e07d7d72f17ed11f7fa7e821b2178cc2d7388e75
Patcharin <gookikps@gmail.com>
2021-08-20 15:59:04 +0700

    commit

--- angular-ctf01/projects/lab4/src/app/lab4component/lab4.component.ts
+++ angular-ctf01/projects/lab4/src/app/lab4component/lab4.component.ts
@@ -5,7 +5,7 @@ templateUrl: './lab4.component.html'
 })
 export class Lab4Component {
     
-    item = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     
 constructor() {}
 }
