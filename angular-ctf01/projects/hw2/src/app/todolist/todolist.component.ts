0c8ef5addce41e830c758d3c7a7882c471a7ca42
Patcharin <gookikps@gmail.com>
2021-08-23 14:51:24 +0700

    commit

---
+++ angular-ctf01/projects/hw2/src/app/todolist/todolist.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+@Component({
+selector: 'app-todolist',
+templateUrl: './todolist.component.html',
+styleUrls: ['./todolist.component.css']
+})
+export class TodoListComponent {
+constructor() {}
+}
